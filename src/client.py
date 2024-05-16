ebe3bd5ecefc1e25c32ecbb7281cd66b5476cc39
Minh Khang <94234570+khanglag@users.noreply.github.com>
2024-05-17 00:05:13 +0700

    khó quá thì mình không cần lượt

--- src/client.py
+++ src/client.py
@@ -26,10 +26,10 @@ def receive_data(client, gs, validMoves):
             if data:
                 move = pickle.loads(data)
                 print(f"Nước đi từ đối thủ: {move.getChessNotation()}")
-                # gs.makeMove(move)
-                # moveMade = True
-                # animate = True
-                # print("playerOne: " + playerOne)
+                gs.makeMove(move)
+                moveMade = True
+                animate = True
+
         except Exception as e:
             print("Mất kết nối với server")
             print(f"Error: {e}")
@@ -42,6 +42,104 @@ def send_data(client, data):
         client.send(serialized_data)
     except Exception as e:
         print(f"Không thể gửi dữ liệu: {e}")
+
+# def main():
+#     pygame.init()
+#     screen = pygame.display.set_mode((WIDTH, HEIGHT))
+#     clock = pygame.time.Clock()
+#     screen.fill(pygame.Color("white"))
+#
+#     gs = engine.GameState()
+#     validMoves = gs.getValidMoves()
+#     moveMade = False
+#     animate = False
+#     loadImages()
+#     run = True
+#     sqSelected = ()
+#     playerClicks = []
+#     gameOver = False
+#     playerOne = True
+#     playerTwo = False
+#
+#
+#     # Kết nối tới server
+#     HOST = '192.168.1.20'  # Địa chỉ IP của server
+#     PORT = 5050
+#
+#     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     client.connect((HOST, PORT))
+#
+#     # Bắt đầu luồng để nhận dữ liệu từ server
+#     receive_thread = threading.Thread(target=receive_data, args=(client, gs, validMoves))
+#     receive_thread.start()
+#
+#     while run:
+#         humanTurn = (gs.whiteToMove and playerOne) or (not gs.whiteToMove and playerTwo)
+#         for event in pygame.event.get():
+#             if event.type == pygame.QUIT:
+#                 run = False
+#                 client.close()
+#             elif event.type == pygame.MOUSEBUTTONDOWN:
+#                 if not gameOver and humanTurn:
+#                     location = pygame.mouse.get_pos()
+#                     col = location[0] // SQ_SIZE
+#                     row = location[1] // SQ_SIZE
+#                     if sqSelected == (row, col):
+#                         sqSelected = ()
+#                         playerClicks = []
+#                     else:
+#                         sqSelected = (row, col)
+#                         playerClicks.append(sqSelected)
+#                     if len(playerClicks) == 2:
+#                         move = engine.Move(playerClicks[0], playerClicks[1], gs.board)
+#                         for i in range(len(validMoves)):
+#                             if move == validMoves[i]:
+#                                 gs.makeMove(validMoves[i])
+#                                 send_data(client, validMoves[i])
+#                                 moveMade = True
+#                                 animate = True
+#                                 sqSelected = ()
+#                                 playerClicks = []
+#                                 playerOne = not playerOne
+#                                 playerTwo = not playerTwo
+#                         if not moveMade:
+#                             playerClicks = [sqSelected]
+#             elif event.type == pygame.KEYDOWN:
+#                 if event.key == pygame.K_z:
+#                     gs.undoMove()
+#                     moveMade = True
+#                     animate = False
+#                 if event.key == pygame.K_r:
+#                     gs = engine.GameState()
+#                     validMoves = gs.getValidMoves()
+#                     sqSelected = ()
+#                     playerClicks = []
+#                     moveMade = False
+#                     animate = False
+#
+#         if moveMade:
+#             if animate:
+#                 animateMove(gs.moveLog[-1], screen, gs.board, clock)
+#             validMoves = gs.getValidMoves()
+#             moveMade = False
+#             animate = False
+#
+#         drawGameState(screen, gs, validMoves, sqSelected)
+#
+#         if gs.checkmate:
+#             gameOver = True
+#             if gs.whiteToMove:
+#                 drawText(screen, "Black wins by checkmate", pygame.Color("black"), (WIDTH // 2, HEIGHT // 2))
+#             else:
+#                 drawText(screen, "White wins by checkmate", pygame.Color("black"), (WIDTH // 2, HEIGHT // 2))
+#         elif gs.stalemate:
+#             gameOver = True
+#             drawText(screen, "Stalemate", pygame.Color("black"), (WIDTH // 2, HEIGHT // 2))
+#
+#         clock.tick(MAX_FPS)
+#         pygame.display.flip()
+#
+#     pygame.quit()
 
 def main():
     pygame.init()
@@ -58,8 +156,8 @@ def main():
     sqSelected = ()
     playerClicks = []
     gameOver = False
-    playerOne = True
-    playerTwo = False
+    # playerOne = True
+    # playerTwo = False
 
 
     # Kết nối tới server
@@ -74,13 +172,13 @@ def main():
     receive_thread.start()
 
     while run:
-        humanTurn = (gs.whiteToMove and playerOne) or (not gs.whiteToMove and playerTwo)
+        # humanTurn = (gs.whiteToMove and playerOne) or (not gs.whiteToMove and playerTwo)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 client.close()
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                if not gameOver and humanTurn:
+                if not gameOver : #and humanTurn:
                     location = pygame.mouse.get_pos()
                     col = location[0] // SQ_SIZE
                     row = location[1] // SQ_SIZE
@@ -100,8 +198,8 @@ def main():
                                 animate = True
                                 sqSelected = ()
                                 playerClicks = []
-                                playerOne = not playerOne
-                                playerTwo = not playerTwo
+                                # playerOne = not playerOne
+                                # playerTwo = not playerTwo
                         if not moveMade:
                             playerClicks = [sqSelected]
             elif event.type == pygame.KEYDOWN:
